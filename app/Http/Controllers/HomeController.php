94a23303a988b56e25119f94469c7d9337ecaf84
Nataniel Fiuza <natan.fiuza@gmail.com>
2025-04-18 23:09:49 -0300

    Exibe postagens pelo banco de dados

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
@@ -13,60 +14,19 @@ class HomeController extends Controller
     public function index()
     {
 
-        $posts = json_decode(Storage::get('data\posts.json'),true);
+        // $posts = json_decode(Storage::get('data\posts.json'),true);
 
-        
+        $posts = Post::orderByRaw('created_at DESC')->get();
+
+        $featured_post = true;
+        foreach ($posts as &$value) {
+            $value->featured_post = $featured_post;
+            $featured_post = false;
+        }
+
         return Inertia::render('Home', [
             'posts' => $posts, // Passa os posts para a view
         ]);
 
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
